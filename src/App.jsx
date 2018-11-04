a9e7e1da8bff2fbe76dc884bfe20c2f24611f8c3
Nima Boscarino <nima.boscarino@gmail.com>
2018-11-04 12:08:09 -0800

    standard js

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,55 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react'
 
 const songLyrics = [
   `Very rarely do you catch me out 'all done specially invited guest'd me out`,
   `Y'all been tellin' jokes that's gon' stress me out
   Soon as I walk in, I'm like, "Let's be out"`,
   `I was off the chain, I was often drained
-  I was off the meds, I was called insane`,
+  I was off the meds, I was called insane`
 ]
 
-
 class App extends Component {
-  constructor(props) {
+  constructor (props) {
     super(props)
 
     this.state = {
       latestLyric: 0
     }
 
-    this.updateLatestLyric = this.updateLatestLyric.bind(this);
+    this.updateLatestLyric = this.updateLatestLyric.bind(this)
   }
 
-  updateLatestLyric(index) { // updateLatestLyric(100) -> state.latestLyric = 100
+  updateLatestLyric (index) { // updateLatestLyric(100) -> state.latestLyric = 100
     this.setState({
       latestLyric: index
     })
   }
 
-  render() {
+  render () {
     return (
       <div>
         <h1>Latest Lyric {this.state.latestLyric}</h1>
-        <SongLyricContainer cb={this.updateLatestLyric}/>
-        <SongLyricContainer cb={this.updateLatestLyric}/>
-        <SongLyricContainer cb={this.updateLatestLyric}/>
-      </div>      
-    );
+        <SongLyricContainer cb={this.updateLatestLyric} />
+        <SongLyricContainer cb={this.updateLatestLyric} />
+        <SongLyricContainer cb={this.updateLatestLyric} />
+      </div>
+    )
   }
 }
 
 class SongLyricContainer extends Component {
-  constructor(props) {
+  constructor (props) {
     super(props)
 
     this.stateasdasd = {
       lyricIndex: 0
     }
-    
-    this.nextLyric = this.nextLyric.bind(this);
-    this.previousLyric = this.previousLyric.bind(this);
+
+    this.nextLyric = this.nextLyric.bind(this)
+    this.previousLyric = this.previousLyric.bind(this)
   }
-  
-  previousLyric() {
+
+  previousLyric () {
     let nextIndex = this.stateasdasd.lyricIndex - 1
     this.setState({
       lyricIndex: nextIndex
@@ -59,7 +58,7 @@ class SongLyricContainer extends Component {
     this.props.cb(nextIndex)
   }
 
-  nextLyric() {
+  nextLyric () {
     let nextIndex = this.stateasdasd.lyricIndex + 1
     this.setState({
       lyricIndex: nextIndex
@@ -68,7 +67,7 @@ class SongLyricContainer extends Component {
     this.props.cb(nextIndex)
   }
 
-  render() {
+  render () {
     return (
       <div>
         <div>
@@ -81,9 +80,6 @@ class SongLyricContainer extends Component {
   }
 }
 
-
-
-export default App;
-
+export default App
 
 // const Welcome = (props) => <button>Howdy doo{props.name}</button>
